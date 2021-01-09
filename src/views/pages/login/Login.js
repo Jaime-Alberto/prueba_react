e983c7603849a271fe597f71ccd4e652452e673d
jaime alberto <jaimearbelaezesquivel2@gmail.com>
2021-01-09 09:27:22 -0500

    subiendo prueba de react

---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CInput,
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CRow
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import axios from "axios";
+
+const baseUrl="http://test.elecsis.co/auth/";
+
+class Login extends React.Component{
+
+constructor(props){
+  super(props)
+}
+
+  state={
+    form:{
+        "username": '',
+        "password": ''
+    },
+    error:false,
+    errorMsg:""
+}
+
+handleChange=async e=>{
+  await this.setState({
+      form:{
+          ...this.state.form,
+          [e.target.name]: e.target.value
+      }
+  });
+  console.log(this.state.form)
+}
+
+manejadorboton = () => {
+let url = baseUrl;
+axios.post(url,this.state.form)
+.then( response =>{
+  console.log(response);
+  if(response.statusText === "OK"){
+    //localStorage.setItem("token", response.data.result.token);
+    this.props.history.push("/dashboard")
+  //}else{
+    //this.setState({
+     // error: true,
+      //errorMsg: response.data.result.error_msg
+    //})
+  }
+})
+}
+
+  render(){
+  return (
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="8">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="text" placeholder="Username" autoComplete="username" name="username" onChange={this.handleChange}/>
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder="Password" autoComplete="current-password" name="password"  onChange={this.handleChange}/>
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6">
+                        <CButton color="primary" className="px-4" onClick={this.manejadorboton}>Login</CButton>
+                      </CCol>
+                      {this.state.error === true &&
+                      <div className="alert alert-danger" role="alert">
+                      {this.state.errorMsg}
+                      </div>
+                      }
+                      <CCol xs="6" className="text-right">
+                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.</p>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+  }
+}
+
+export default Login
